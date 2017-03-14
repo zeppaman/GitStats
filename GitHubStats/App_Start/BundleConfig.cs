613523f8aa52c2de24c964c7844ef5ab3b8a0b25
Daniele Fontani <d.fontani@sintraconsulting.eu>
2017-03-14 23:58:34 +0100

    added home page

--- GitHubStats/App_Start/BundleConfig.cs
+++ GitHubStats/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace GitHubStats
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                       "~/Content/font-awesome.min.css"));
         }
     }
 }
