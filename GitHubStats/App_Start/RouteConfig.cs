e23deb63c84fb5fdb65574b92adc51a9797a9450
Daniele Fontani <d.fontani@sintraconsulting.eu>
2017-03-21 08:17:05 +0100

    added badges

--- GitHubStats/App_Start/RouteConfig.cs
+++ GitHubStats/App_Start/RouteConfig.cs
@@ -13,11 +13,18 @@ namespace GitHubStats
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("SVG", "{controller}/{action}/{id}.svg", new { controller = "Home", action = "Index",id=-10 });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
+
+
             );
+
+           
         }
     }
 }
---
+++ GitHubStats/Controllers/BadgesController.cs
@@ -0,0 +1,68 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubStats.Controllers
+{
+    public class BadgesController : Controller
+    {
+        const string badgeTemplate = "https://img.shields.io/badge/{0}-{1}-{2}.svg";
+        // GET: Badges
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult RepositoryDownloads( long id=0)
+        {
+           if (id == 0) throw new Exception("Repository Id Missing");
+            string url = "";
+
+            GitHubClient client = new GitHubClient(new ProductHeaderValue(global::GitHubStats.Controllers.AppConfig.Current.AppName));
+
+            var total = 0;
+            var repository = client.Repository.Get(id).Result;
+            foreach(var rel in client.Repository.Release.GetAll(id).Result)
+            {
+                foreach (var asset in rel.Assets)
+                {
+                    total += asset.DownloadCount;
+
+                }
+            }
+
+            url = string.Format(badgeTemplate, "downloads", total, "orange");
+
+
+            return DownloadFile(url, "repositoryDownload.svg", true);
+           
+        }
+
+
+        private ActionResult DownloadFile(string url,string filename, bool inline)
+        {
+         
+
+            WebClient client = new WebClient();
+            var bytes=client.DownloadData(url);
+          
+            string contentType = MimeMapping.GetMimeMapping(filename);
+
+            var cd = new System.Net.Mime.ContentDisposition
+            {
+                FileName = filename,
+                Inline = inline,
+            };
+
+            Response.AppendHeader("Content-Disposition", cd.ToString());
+
+            return File(bytes, contentType);
+        }
+    }
+
+}
