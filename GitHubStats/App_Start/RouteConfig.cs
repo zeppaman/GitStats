ef8f8ed01d21eb457070a7e6856d6c2e91c784e6
Daniele Fontani <d.fontani@sintraconsulting.eu>
2017-03-22 18:36:35 +0100

    route fixed

--- GitHubStats/App_Start/RouteConfig.cs
+++ GitHubStats/App_Start/RouteConfig.cs
@@ -17,8 +17,8 @@ namespace GitHubStats
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" }
 
 
 
--- GitHubStats/Controllers/OAuthController.cs
+++ GitHubStats/Controllers/OAuthController.cs
@@ -74,7 +74,7 @@ namespace GitHubStats.Controllers
 
 
 
-                return RedirectToAction("Index", "Stats");
+               return new RedirectResult("~/Stats/");
             }
             catch (Exception exc)
             {
